dfa4f9d6cf90c95a5d2291b30e100393bebe5b93
Ryan Goldstein <ryan@ryanisaacg.com>
2022-12-25 22:00:43 -0500

    Add a test to ensure that shared and unique aren't interchangeable

--- tests/references.rs
+++ tests/references.rs
@@ -21,6 +21,29 @@ fn test(): i32 {
 }
 
 #[test]
+#[should_panic]
+fn reject_wrong_kind() {
+    assert_eq!(
+        1,
+        run_test(
+            r#"
+fn test(): i32 {
+    let a = 5
+    mutate(shared a, 1)
+    a
+}
+
+fn mutate(ptr: unique i32, val: i32) {
+    ptr = val;
+}
+"#,
+            ()
+        )
+        .unwrap()
+    );
+}
+
+#[test]
 fn mutation_auto_dereference() {
     assert_eq!(
         6,
