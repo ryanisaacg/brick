4809b0703cccef67a3438cb0beb4f615c620a640
Ryan <ryanisaacg@users.noreply.github.com>
2023-11-23 00:11:09 -0500

    IO example

--- src/main.rs
+++ src/main.rs
@@ -1,11 +1,18 @@
 use brick::{interpret_code, interpreter::ExternBinding, Value};
-use std::{collections::HashMap, fs::read_to_string};
+use std::{collections::HashMap, fs::read_to_string, io::stdin};
 
 fn main() {
     let mut bindings: HashMap<String, &ExternBinding> = HashMap::new();
-    bindings.insert("print".to_string(), &|values| {
-        let Value::Float(input) = values[0] else {
-            panic!("expected float");
+    bindings.insert("read".to_string(), &|_| {
+        let mut input = String::new();
+        stdin()
+            .read_line(&mut input)
+            .expect("should be able to read from stdin");
+        vec![Value::Int(input.trim().parse().expect("should be an int"))]
+    });
+    bindings.insert("write".to_string(), &|values| {
+        let Value::Int(input) = values[0] else {
+            panic!("expected int");
         };
         println!("{}", input);
         vec![]
