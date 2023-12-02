d5e5cf1221be9a624685a0ec0a93bdb3399452c5
Ryan <ryanisaacg@users.noreply.github.com>
2023-12-02 00:52:00 -0500

    Add some basic tests that functions work

---
+++ tests/functions.rs
@@ -0,0 +1,37 @@
+use assert_matches::assert_matches;
+use brick::{eval, Value};
+
+#[tokio::test]
+async fn add() {
+    let result = eval(r#"
+fn add(a: i32, b: i32): i32 {
+    a + b
+}
+
+add(1, 2)
+"#)
+    .await
+    .unwrap();
+    assert_matches!(&result[..], [Value::Int(3)]);
+}
+
+#[tokio::test]
+async fn recursion() {
+    let result = eval(r#"
+fn fib(input: i32): i32 {
+    if input < 1 {
+        return 0;
+    }
+    if input == 1 {
+        return 1;
+    }
+
+    fib(input - 1) + fib(input - 2)
+}
+
+fib(5)
+"#)
+    .await
+    .unwrap();
+    assert_matches!(&result[..], [Value::Int(5)]);
+}
