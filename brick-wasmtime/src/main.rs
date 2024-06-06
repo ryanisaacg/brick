d4723c4b89caba1bb0079f6202436d6490cbef5e
Ryan <ryanisaacg@users.noreply.github.com>
2024-06-05 20:39:23 -0400

    Start playing around with SDL2 bindings

--- brick-wasmtime/src/main.rs
+++ brick-wasmtime/src/main.rs
@@ -1,8 +1,14 @@
-use std::env;
+use std::{
+    collections::HashSet,
+    env,
+    sync::{mpsc::TryRecvError, Arc, Mutex},
+};
 
+use anyhow::{bail, Context};
 use brick_wasm_backend::compile;
 use brick_wasmtime::add_runtime_functions;
-use wasmtime::{Engine, Linker, Module, Store};
+use sdl2::{event::Event, pixels::Color, rect::Rect};
+use wasmtime::{Engine, Linker, Module, Store, Val};
 
 fn main() -> anyhow::Result<()> {
     let mut args = env::args();
@@ -15,12 +21,121 @@ fn main() -> anyhow::Result<()> {
     let mut store = Store::new(&engine, ());
     let mut linker = Linker::new(&engine);
     add_runtime_functions(&mut linker)?;
-    linker.func_wrap("bindings", "print", move |val: i32| {
-        println!("{val}");
+
+    let (send_draw_command, recv_draw_command) = std::sync::mpsc::channel();
+    let down_keys = Arc::new(Mutex::new(HashSet::new()));
+
+    let send_cmd = send_draw_command.clone();
+    linker.func_wrap("bindings", "clear", move || {
+        send_cmd.send(DrawCommand::Clear).unwrap();
+    })?;
+    let send_cmd = send_draw_command.clone();
+    linker.func_wrap("bindings", "set_color", move |r, g, b, a| {
+        send_cmd.send(DrawCommand::SetColor(r, g, b, a)).unwrap();
+    })?;
+    let send_cmd = send_draw_command.clone();
+    linker.func_wrap("bindings", "present", move || {
+        send_cmd.send(DrawCommand::Present).unwrap();
+    })?;
+    let send_cmd = send_draw_command.clone();
+    linker.func_wrap("bindings", "fill_rect", move |x, y, w, h| {
+        send_cmd.send(DrawCommand::DrawRect(x, y, w, h)).unwrap();
+    })?;
+    let keys = down_keys.clone();
+    linker.func_wrap("bindings", "is_key_down", move |key: i32| {
+        let keys = keys.lock().unwrap();
+        if keys.contains(&key) {
+            1
+        } else {
+            0
+        }
     })?;
 
     let module = Module::from_binary(&engine, binary.as_slice())?;
-    linker.instantiate(&mut store, &module)?;
+    let instance = linker.instantiate(&mut store, &module)?;
+
+    let init_func = instance
+        .get_func(&mut store, "init")
+        .context("failed to find init")?;
+    let tick_func = instance
+        .get_func(&mut store, "tick")
+        .context("failed to find tick")?;
+
+    let sdl_context = sdl2::init().unwrap();
+    let video_subsystem = sdl_context.video().unwrap();
+
+    let window = video_subsystem
+        .window("Brick demo", 800, 600)
+        .position_centered()
+        .build()
+        .unwrap();
+
+    let mut canvas = window.into_canvas().build().unwrap();
+    let mut event_pump = sdl_context.event_pump().unwrap();
+
+    let mut results = [Val::I32(0)];
+    init_func.call(&mut store, &[], &mut results)?;
+
+    let ctx_pointer = results[0].clone();
+
+    let mut running = true;
+    while running {
+        for event in event_pump.poll_iter() {
+            match event {
+                Event::Quit { .. } => {
+                    running = false;
+                }
+                Event::KeyDown {
+                    keycode: Some(keycode),
+                    repeat: false,
+                    ..
+                } => {
+                    let key = keycode as i32;
+                    let mut keys = down_keys.lock().unwrap();
+                    keys.insert(key);
+                }
+                Event::KeyUp {
+                    keycode: Some(keycode),
+                    repeat: false,
+                    ..
+                } => {
+                    let key = keycode as i32;
+                    let mut keys = down_keys.lock().unwrap();
+                    keys.remove(&key);
+                }
+                _ => {}
+            }
+        }
+
+        tick_func.call(&mut store, &[ctx_pointer.clone()], &mut [])?;
+
+        loop {
+            let event = match recv_draw_command.try_recv() {
+                Ok(event) => event,
+                Err(TryRecvError::Empty) => break,
+                Err(TryRecvError::Disconnected) => bail!("Event loop disconnected"),
+            };
+            match event {
+                DrawCommand::Clear => canvas.clear(),
+                DrawCommand::Present => canvas.present(),
+                DrawCommand::SetColor(r, g, b, a) => {
+                    canvas.set_draw_color(Color::RGBA(r as u8, g as u8, b as u8, a as u8));
+                }
+                DrawCommand::DrawRect(x, y, w, h) => {
+                    canvas
+                        .fill_rect(Rect::new(x, y, w as u32, h as u32))
+                        .unwrap();
+                }
+            }
+        }
+    }
 
     Ok(())
+}
+
+enum DrawCommand {
+    Clear,
+    Present,
+    SetColor(i32, i32, i32, i32),
+    DrawRect(i32, i32, i32, i32),
 }
